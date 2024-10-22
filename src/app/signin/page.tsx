3ea74cf4a64c21bdacc2c991b22ebfc2537f025e
Vilas3 <gabriel.vilas@usp.br>
2024-10-22 16:28:31 -0300

    Header

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -4,6 +4,5 @@ import { Header } from '../../components/Header';
 export default function Signin(){
     return <>
         <Header page="About" />
-        <h1>Oi</h1>
     </>
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,25 @@
 import Link from "next/link";
-import React, { Component } from 'react';
-import mosquito from '../app/images/mosquito.png';
-
+import React from 'react';
 import styles from "./Header.module.css";
 
+import Image from 'next/image';
+import mosquito from "../public/images/mosquito.png";
+
 type HeaderProps = {
-    page: string
+     page: string
 }
 
 export function Header({ page }: HeaderProps) {
-    return <>
+    return (
         <div className={styles.header_wrapper}>
-            <div className="">
-                <h1 className="">
-                        Tchau Dengue
-                </h1>
-
-                <img src={mosquito} />
-
+            <div className={styles.logo_title_wrapper}>
+                <Image src={mosquito} alt="Mosquito" width={24} height={24} />
+                <h1 className={styles.title}>Tchau Dengue</h1>
             </div>
-
-            <div className="">
-                <Link
-                    className={styles.sign_in}
-         
-                    href="/signin">Sign in</Link>
-                <Link
-                className={styles.register} 
-                href="/register">Register</Link>
+            <div className={styles.buttons_wrapper}>
+                <Link className={styles.sign_in} href="/signin">Sign in</Link>
+                <Link className={styles.register} href="/register">Register</Link>
             </div>
-
         </div>
-    </>
-
+    );
 }
