20244157492f6e00442155b0bc73f76ef31ef079
Henrique-God <henriquergodoy07@gmail.com>
2024-10-22 17:33:40 -0300

    home page added

--- src/app/dashboard/home/layout.tsx
+++ src/app/dashboard/home/layout.tsx
@@ -1,12 +1,16 @@
 import SideNav from '@/src/app/components/sidenav';
+import Footer from '../../components/footer';
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-40">
-        <SideNav />
+    <div>
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+        <div className="w-full flex-none md:w-40">
+          <SideNav />
+        </div>
+        <div className="flex-grow md:overflow-y-auto pl-8">{children}</div>
       </div>
-      <div className="flex-grow md:overflow-y-auto pl-8">{children}</div>
+      <Footer></Footer>
     </div>
   );
 }
