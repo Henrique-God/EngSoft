28298404034056e3cd5ac6e36854061f759eec5c
gabrielzitoo <gabriel08.set.2000@gmail.com>
2024-12-01 19:57:34 -0300

    added getpage

--- src/app/components/conecctionBackendWiki.tsx
+++ src/app/components/conecctionBackendWiki.tsx
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:5036/";
+const baseUrl = "http://localhost:5036/wiki-pages/";
 
 export interface ImgInsertResponse {
     success: boolean;
@@ -8,7 +8,7 @@ export interface ImgInsertResponse {
 
 export async function ImgInsertHandler(image: string): Promise<ImgInsertResponse> {
     try {
-        const url = new URL(`${baseUrl}wiki/images/insert`);
+        const url = new URL(`${baseUrl}add-photo`);
         url.searchParams.append("image", image);
         const token = localStorage.getItem("token");
         const response = await fetch(url.toString(), {
@@ -22,9 +22,9 @@ export async function ImgInsertHandler(image: string): Promise<ImgInsertResponse
         if (!response.ok) {
             return { success: false, error: response.statusText };
         }
-        const data = await response.json();
+        const data = await response.json(); 
 
-        return { success: true, path: data.path };
+        return { success: true, path: data.picture };
     } catch (error) {
         console.error("Error while logging in:", error);
         return { success: false, error: (error as Error).message };
@@ -41,9 +41,9 @@ export interface CreateWikiResponse {
 
 export async function CreateWikitHandler(formData: { wikiTitle: string; wikiText: string }): Promise<CreateWikiResponse> {
     try {
-        const url = new URL(`${baseUrl}wikipages`);
-        url.searchParams.append("wikiTitle", formData.wikiTitle);
-        url.searchParams.append("wikiText", formData.wikiText);
+        const url = new URL(`${baseUrl}create-page`);
+        url.searchParams.append("PageText", formData.wikiText);
+        url.searchParams.append("PageTitle", formData.wikiTitle);
         const token = localStorage.getItem("token");
         const response = await fetch(url.toString(), {
             method: "POST",
@@ -58,7 +58,39 @@ export async function CreateWikitHandler(formData: { wikiTitle: string; wikiText
         }
         const data = await response.json();
 
-        return { success: true, title: data.title};
+        return { success: true, title: data[0].PageTitle};
+    } catch (error) {
+        console.error("Error while logging in:", error);
+        return { success: false, error: (error as Error).message };
+    }
+}
+
+
+export interface GetWikiResponse {
+    success: boolean;
+    error?: string;
+    content?: string;
+}
+
+export async function GetWikitHandler(wikiTitle: string): Promise<GetWikiResponse> {
+    try {
+        const url = new URL(`${baseUrl}search-pages/`);
+        url.searchParams.append("PageTitle", wikiTitle);
+        const token = localStorage.getItem("token");
+        const response = await fetch(url.toString(), {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`,
+
+            },
+        });
+        if (!response.ok) {
+            return { success: false, error: response.statusText };
+        }
+        const data = await response.json();
+
+        return { success: true, content: data[0]};
     } catch (error) {
         console.error("Error while logging in:", error);
         return { success: false, error: (error as Error).message };
